71616b8402b0c55973b74b2e5174dda06fecc998
Priyadarshini_Mahadevan <123006258+priyadivya24@users.noreply.github.com>
2024-02-05 14:16:12 +0100

    Add files via upload

--- Visual_Fastarrow.py
+++ Visual_Fastarrow.py
@@ -32,15 +32,19 @@ def get_dropdown_options(main_folders, sub_folders, subsub_folder):
     return [{'label': folder, 'value': folder} for folder in subsubsubfolders]
 
 
-def get_initial_date(main_folders, sub_folders, subsub_folder):
+def get_initial_date_range(main_folders, sub_folders, subsub_folder):
     subsubsubfolder_path = os.path.join(main_folders, sub_folders, subsub_folder)
     files = [f for f in os.listdir(subsubsubfolder_path) if f.endswith('.parquet')]
     if not files:
-        return None
-    first_file = os.path.join(subsubsubfolder_path, files[0])
-    df = fastparquet.ParquetFile(first_file).to_pandas()
-    initial_date = df['timestamp'].min()
-    return initial_date
+        return None, None
+    all_dates = []
+    for file in files:
+        file_path = os.path.join(subsubsubfolder_path, file)
+        df = fastparquet.ParquetFile(file_path).to_pandas()
+        all_dates.extend(df['timestamp'])
+    min_date = min(all_dates)
+    max_date = max(all_dates)
+    return min_date, max_date
 
 
 def convert_timestamp(df):
@@ -53,15 +57,19 @@ def downsample_data(df, timestamp_col='timestamp', value_col='data'):
     return df_resampled
 
 
-def update_line_plot(selected_subfolder, selected_date, main_folders, sub_folders, subsub_folder):
+def update_line_plot(selected_subfolder, start_date, end_date, main_folders, sub_folders, subsub_folder):
     if selected_subfolder is None:
         return px.line()
 
     subfolder_path = os.path.join(main_folders, sub_folders, subsub_folder, selected_subfolder)
     df = read_parquet_files(subfolder_path)
     df = convert_timestamp(df)
-    if selected_date:
-        df = df[df['timestamp'].dt.date == selected_date]
+    if start_date and end_date:
+        df = df[(df['timestamp'] >= start_date) & (df['timestamp'] <= end_date)]
+    elif start_date:
+        df = df[df['timestamp'] >= start_date]
+    elif end_date:
+        df = df[df['timestamp'] >= end_date]
     df = downsample_data(df)
 
     fig = make_subplots(specs=[[{'secondary_y': True}]])
@@ -76,12 +84,19 @@ def create_layout(main_folders, sub_folders, subsub_folders_1, sub_folders2, sub
         html.H1("Dashboard"),
         html.Div([
             html.H2("XTIN.MLO1 and SLO1 Dashboards"),
+            dcc.DatePickerRange(
+                id='date1',
+                display_format='YYYY-MM-DD',
+                start_date=None,
+                end_date=None,
+                style={'margin-bottom': '20px'}
+            ),
             dbc.Row([
                 dbc.Col([
                     dcc.Dropdown(
                         id='subfolder-dropdown',
                         options=get_dropdown_options(main_folders, sub_folders, subsub_folders_1),
-                        value=None,
+                        value='CTRL0.OUT.MEAN.RD',
                         placeholder="Select Subfolder"
                     ),
                 ], width=6),
@@ -89,7 +104,7 @@ def create_layout(main_folders, sub_folders, subsub_folders_1, sub_folders2, sub
                     dcc.Dropdown(
                         id='subfolder-dropdown-2',
                         options=get_dropdown_options(main_folders, sub_folders, subsub_folders_2),
-                        value=None,
+                        value='CTRL0.OUT.MEAN.RD',
                         placeholder="Select Subfolder"
                     ),
                 ], width=6),
@@ -101,12 +116,19 @@ def create_layout(main_folders, sub_folders, subsub_folders_1, sub_folders2, sub
         ]),
         html.Div([
             html.H2("LSU"),
+            dcc.DatePickerRange(
+                id='date2',
+                display_format='YYYY-MM-DD',
+                start_date=None,
+                end_date=None,
+                style={'margin-bottom': '20px'}
+            ),
             dbc.Row([
                 dbc.Col([
                     dcc.Dropdown(
                         id='subfolder-dropdown-3',
                         options=get_dropdown_options(main_folders, sub_folders2, subsub_folders_3),
-                        value=None,
+                        value='FMC1.MD22.0.POSITION.RD',
                         placeholder="Select Subfolder"
                     ),
                 ], width=6),
@@ -114,7 +136,7 @@ def create_layout(main_folders, sub_folders, subsub_folders_1, sub_folders2, sub
                     dcc.Dropdown(
                         id='subfolder-dropdown-4',
                         options=get_dropdown_options(main_folders, sub_folders2, subsub_folders_4),
-                        value=None,
+                        value='LSU.2.CTRL_IN.MEAN.RD',
                         placeholder="Select Subfolder"
                     ),
                 ], width=6),
@@ -143,33 +165,37 @@ app.layout = create_layout(main_folder, sub_folder, subsub_folder_1, sub_folder2
 
 @app.callback(Output('line-plot-1', 'figure'),
               [Input('subfolder-dropdown', 'value'),
-               Input('date', 'date')])
-def update_graph_1(selected_subfolder, selected_date):
-    fig = update_line_plot(selected_subfolder, selected_date, main_folder, sub_folder, subsub_folder_1)
+               Input('date1', 'start_date'),
+               Input('date1', 'end_date')])
+def update_graph_1(selected_subfolder, start_date, end_date):
+    fig = update_line_plot(selected_subfolder, start_date, end_date, main_folder, sub_folder, subsub_folder_1)
     return fig
 
 
 @app.callback(Output('line-plot-2', 'figure'),
               [Input('subfolder-dropdown-2', 'value'),
-               Input('date', 'date')])
-def update_graph_2(selected_subfolder, selected_date):
-    fig = update_line_plot(selected_subfolder, selected_date, main_folder, sub_folder, subsub_folder_2)
+               Input('date1', 'start_date'),
+               Input('date1', 'end_date')])
+def update_graph_2(selected_subfolder, start_date, end_date):
+    fig = update_line_plot(selected_subfolder, start_date, end_date, main_folder, sub_folder, subsub_folder_2)
     return fig
 
 
 @app.callback(Output('line-plot-3', 'figure'),
               [Input('subfolder-dropdown-3', 'value'),
-               Input('date', 'date')])
-def update_graph_3(selected_subfolder, selected_date):
-    fig = update_line_plot(selected_subfolder, selected_date, main_folder, sub_folder2, subsub_folder_3)
+               Input('date2', 'start_date'),
+               Input('date2', 'end_date')])
+def update_graph_3(selected_subfolder, start_date, end_date):
+    fig = update_line_plot(selected_subfolder, start_date, end_date, main_folder, sub_folder2, subsub_folder_3)
     return fig
 
 
 @app.callback(Output('line-plot-4', 'figure'),
               [Input('subfolder-dropdown-4', 'value'),
-               Input('date', 'date')])
-def update_graph_4(selected_subfolder, selected_date):
-    fig = update_line_plot(selected_subfolder, selected_date, main_folder, sub_folder2, subsub_folder_4)
+               Input('date2', 'start_date'),
+               Input('date2', 'end_date')])
+def update_graph_4(selected_subfolder, start_date, end_date):
+    fig = update_line_plot(selected_subfolder, start_date, end_date, main_folder, sub_folder2, subsub_folder_4)
     return fig
 
 
