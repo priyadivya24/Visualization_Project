07b1036f9fbecdc0a9f43ee4c21dbce70fc7ae4b
Priyadarshini_Mahadevan <123006258+priyadivya24@users.noreply.github.com>
2024-02-07 17:37:56 +0100

    Update matploylib2.py

--- matploylib_pyarrow.py
+++ matploylib_pyarrow.py
@@ -1,166 +1,62 @@
-import datetime
 import os
-import time
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import datetime
+from tkinter import Tk, StringVar, OptionMenu, LabelFrame
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-import dash
-from dash import dcc
-from dash import html
-import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output
+# Function to read parquet files and convert timestamp
+def read_parquet_and_convert_timestamp(directory):
+    data = pd.DataFrame()
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".parquet"):
+                filepath = os.path.join(root, file)
+                df = pd.read_parquet(filepath)
+                df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s')  # Assuming timestamp is in seconds
+                data = pd.concat([data, df])
+    return data
 
-import pyarrow.parquet as pq
-import pyarrow as pa
+# Function to update plot based on selected subsubsubfolders
+def update_plot(*args):
+    selected_xtin = xtin_var.get()
+    selected_xhexp = xhexp_var.get()
+    if selected_xtin and selected_xhexp:
+        xtin_df = read_parquet_and_convert_timestamp(os.path.join('XFEL.SYNC', 'LASER.LOCK.XLO', 'XTIN.MLO1', selected_xtin))
+        xhexp_df = read_parquet_and_convert_timestamp(os.path.join('XFEL.SYNC', 'LASER.LOCK.XLO', 'XHEXP1.SLO1', selected_xhexp))
+        
+        fig, ax = plt.subplots()
+        ax.plot(xtin_df['timestamp'], xtin_df['data'], label='XTIN.MLO1')
+        ax.plot(xhexp_df['timestamp'], xhexp_df['data'], label='XHEXP1.SLO1')
+        ax.set_xlabel('Timestamp')
+        ax.set_ylabel('Data')
+        ax.legend()
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        plt.show()
 
-def read_parquet_files(subfolder_path):
-    files = [f for f in os.listdir(subfolder_path) if f.endswith('.parquet')]
-    tables = [pq.read_table(os.path.join(subfolder_path, f)) for f in files]
-    concatenated_table = pa.concat_tables(tables)
-    return concatenated_table.to_pandas()
+# GUI setup
+root = Tk()
+root.title("Dashboard")
 
-def get_dropdown_options(main_folders, sub_folders, subsub_folder):
-    if None in (main_folders, sub_folders, subsub_folder):
-        return []
+# Dropdowns for selecting subsubsubfolders
+subsubfolders_xtin = [d for d in os.listdir(os.path.join('XFEL.SYNC', 'LASER.LOCK.XLO', 'XTIN.MLO1')) if os.path.isdir(os.path.join('XFEL.SYNC', 'LASER.LOCK.XLO', 'XTIN.MLO1', d))]
+subsubfolders_xhexp = [d for d in os.listdir(os.path.join('XFEL.SYNC', 'LASER.LOCK.XLO', 'XHEXP1.SLO1')) if os.path.isdir(os.path.join('XFEL.SYNC', 'LASER.LOCK.XLO', 'XHEXP1.SLO1', d))]
 
-    main_folders = str(main_folders)
-    sub_folders = str(sub_folders)
-    subsub_folder = str(subsub_folder)
+xtin_var = StringVar(root)
+xtin_var.set(subsubfolders_xtin[0])  # default value
+xhexp_var = StringVar(root)
+xhexp_var.set(subsubfolders_xhexp[0])  # default value
 
-    subsubsubfolder_path = os.path.join(main_folders, sub_folders, subsub_folder)
-    subsubsubfolders = [f for f in os.listdir(subsubsubfolder_path) if os.path.isdir(os.path.join(subsubsubfolder_path, f))]
+xtin_dropdown = OptionMenu(root, xtin_var, *subsubfolders_xtin, command=update_plot)
+xhexp_dropdown = OptionMenu(root, xhexp_var, *subsubfolders_xhexp, command=update_plot)
 
-    return [{'label': folder, 'value': folder} for folder in subsubsubfolders]
+label_frame = LabelFrame(root, text="Select Subsubsubfolders")
+label_frame.pack(padx=10, pady=10)
+xtin_dropdown.pack(in_=label_frame, side="left")
+xhexp_dropdown.pack(in_=label_frame, side="left")
 
-def convert_timestamp(df):
-    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s')
-    return df
+# Initial plot
+update_plot()
 
-def downsample_data(df, timestamp_col='timestamp', value_col='data'):
-    df_resampled = df.groupby(pd.Grouper(key=timestamp_col, freq='6h'))[value_col].mean().reset_index()
-    return df_resampled
-
-def update_line_plot(selected_subfolder, main_folders, sub_folders, subsub_folder):
-    if selected_subfolder is None:
-        return plt.figure()
-    start = time.perf_counter()
-    now = datetime.datetime.now().strftime("%H:%M:%S")
-    print("Read Parquet started : ", now)
-
-    subfolder_path = os.path.join(main_folders, sub_folders, subsub_folder, selected_subfolder)
-    df = read_parquet_files(subfolder_path)
-    now = datetime.datetime.now().strftime("%H:%M:%S")
-    print("Read Parquet completed at : ", now)
-    df = convert_timestamp(df)
-    df = downsample_data(df)
-
-    fig, ax = plt.subplots()
-    ax.plot(df['timestamp'], df['data'])
-    ax.set_xlabel('Timestamp')
-    ax.set_ylabel('Data')
-    ax.set_title('Line Plot')
-    end = time.perf_counter()
-    print(f'Update Line Plot Function Completed in {round(end - start, 2)} seconds')
-
-    return fig
-
-def create_layout(main_folders, sub_folders, subsub_folders_1, sub_folders2, subsub_folders_2, subsub_folders_3, subsub_folders_4):
-    return html.Div([
-        html.H1("Dashboard"),
-        html.Div([
-            html.H2("XTIN.MLO1 and SLO1 Dashboards"),
-            dbc.Row([
-                dbc.Col([
-                    dcc.Dropdown(
-                        id='subfolder-dropdown',
-                        options=get_dropdown_options(main_folders, sub_folders, subsub_folders_1),
-                        value='CTRL0.OUT.MEAN.RD',
-                        placeholder="Select Subfolder"
-                    ),
-                ], width=6),
-                dbc.Col([
-                    dcc.Dropdown(
-                        id='subfolder-dropdown-2',
-                        options=get_dropdown_options(main_folders, sub_folders, subsub_folders_2),
-                        value='CTRL0.OUT.MEAN.RD',
-                        placeholder="Select Subfolder"
-                    ),
-                ], width=6),
-            ]),
-            dbc.Row([
-                dbc.Col(dcc.Loading(html.Div(id='line-plot-1')), width=6),
-                dbc.Col(dcc.Loading(html.Div(id='line-plot-2')), width=6),
-            ]),
-        ]),
-        html.Div([
-            html.H2("LSU"),
-            dbc.Row([
-                dbc.Col([
-                    dcc.Dropdown(
-                        id='subfolder-dropdown-3',
-                        options=get_dropdown_options(main_folders, sub_folders2, subsub_folders_3),
-                        value='FMC1.MD22.0.POSITION.RD',
-                        placeholder="Select Subfolder"
-                    ),
-                ], width=6),
-                dbc.Col([
-                    dcc.Dropdown(
-                        id='subfolder-dropdown-4',
-                        options=get_dropdown_options(main_folders, sub_folders2, subsub_folders_4),
-                        value='LSU.2.CTRL_IN.MEAN.RD',
-                        placeholder="Select Subfolder"
-                    ),
-                ], width=6),
-            ]),
-            dbc.Row([
-                dbc.Col(dcc.Loading(html.Div(id='line-plot-3')), width=6),
-                dbc.Col(dcc.Loading(html.Div(id='line-plot-4')), width=6),
-            ]),
-        ]),
-    ], style={'width': '80%', 'margin': 'auto', 'display': 'block'})
-
-main_folder = 'XFEL.SYNC'
-sub_folder = 'LASER.LOCK.XLO'
-sub_folder2 = 'LINK.LOCK'
-subsub_folder_1 = 'XTIN.MLO1'
-subsub_folder_2 = 'XHEXP1.SLO1'
-subsub_folder_3 = 'XTIN.AMC8.ACTUATOR'
-subsub_folder_4 = 'XTIN.AMC8.CONTROLLER'
-
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX])
-app.layout = create_layout(main_folder, sub_folder, subsub_folder_1, sub_folder2, subsub_folder_2, subsub_folder_3, subsub_folder_4)
-
-@app.callback(Output('line-plot-1', 'children'), [Input('subfolder-dropdown', 'value')])
-def update_graph_1(selected_subfolder):
-    start = time.perf_counter()
-    fig = update_line_plot(selected_subfolder, main_folder, sub_folder, subsub_folder_1)
-    end = time.perf_counter()
-    print(f'Update Graph1 Plot Function Completed in {round(end - start, 2)} seconds')
-    return dcc.Graph(figure=fig)
-
-@app.callback(Output('line-plot-2', 'children'), [Input('subfolder-dropdown-2', 'value')])
-def update_graph_2(selected_subfolder):
-    start = time.perf_counter()
-    fig = update_line_plot(selected_subfolder, main_folder, sub_folder, subsub_folder_2)
-    end = time.perf_counter()
-    print(f'Update Graph2 Plot Function Completed in {round(end - start, 2)} seconds')
-    return dcc.Graph(figure=fig)
-
-@app.callback(Output('line-plot-3', 'children'), [Input('subfolder-dropdown-3', 'value')])
-def update_graph_3(selected_subfolder):
-    start = time.perf_counter()
-    fig = update_line_plot(selected_subfolder, main_folder, sub_folder2, subsub_folder_3)
-    end = time.perf_counter()
-    print(f'Update Graph3 Plot Function Completed in {round(end - start, 2)} seconds')
-    return dcc.Graph(figure=fig)
-
-@app.callback(Output('line-plot-4', 'children'), [Input('subfolder-dropdown-4', 'value')])
-def update_graph_4(selected_subfolder):
-    start = time.perf_counter()
-    fig = update_line_plot(selected_subfolder, main_folder, sub_folder2, subsub_folder_4)
-    end = time.perf_counter()
-    print(f'Update Graph4 Plot Function Completed in {round(end - start, 2)} seconds')
-    return dcc.Graph(figure=fig)
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+root.mainloop()
